eba3d00b6cf0ecc79295d813b8af9d4d8f1f8610
Saeed Sekandari <sekandari2003@gmail.com>
2026-08-06 20:42:05 -0700

    completed selection sort implementation

--- src/SelectionSortArray.java
+++ src/SelectionSortArray.java
@@ -1,2 +1,24 @@
+//Sort this array using selection sort
+//[55,32,8,99,1,15]
+
+import java.util.Arrays;
 public class SelectionSortArray {
+    public static void selectionSort(int[] numbers){
+        for (int i=0; i<numbers.length; i++){
+            int minIndex = i;
+            for(int j=i+1; j<numbers.length;j++){
+                if(numbers[j] < numbers[minIndex]){
+                    minIndex=j;
+                }
+            }
+            int temp = numbers[i];
+            numbers [i] = numbers[minIndex];
+            numbers[minIndex] = temp;
+        }
+    }
+    public static void main(String[] args){
+        int[] numbers = {55,32,8,99,1,15};
+        selectionSort(numbers);
+        System.out.println("The sorted array is: " + Arrays.toString(numbers));
+    }
 }
