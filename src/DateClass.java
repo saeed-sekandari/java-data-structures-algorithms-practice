a4de22360c051210b83afb9a9d4d5579875ade65
Saeed Sekandari <sekandari2003@gmail.com>
2026-08-09 21:34:00 -0700

    Practice how to diaplay the date

--- src/DateClass.java
+++ src/DateClass.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
+import java.util.Date;
 public class DateClass {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+
+    }
 }
