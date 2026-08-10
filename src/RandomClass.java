a99054ccc91f36cf6eea9f8bd87a15a433644a17
Saeed Sekandari <sekandari2003@gmail.com>
2026-08-10 12:51:27 -0700

    Added and tested random class

---
+++ src/RandomClass.java
@@ -0,0 +1,7 @@
+import java.util.Random;
+public class RandomClass {
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt(10));
+    }
+}
