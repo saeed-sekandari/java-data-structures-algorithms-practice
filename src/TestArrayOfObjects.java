56a30110c9e9d1f38576724f17c0ef42d9abde1b
Saeed Sekandari <sekandari2003@gmail.com>
2026-08-10 15:29:00 -0700

    Practiced array of objects

---
+++ src/TestArrayOfObjects.java
@@ -0,0 +1,17 @@
+public class TestArrayOfObjects {
+    public static void main(String[] args) {
+
+        Circle[] circles = new Circle[4];
+
+        for (int i = 0; i < circles.length; i++) {
+            circles[i] = new Circle(i+1);
+        }
+        for(int i=0; i<circles.length;i++){
+            System.out.println("Circle " + (i+1));
+            System.out.println("Radius " + circles[i].radius);
+            System.out.println("Area " + circles[i].getArea());
+            System.out.println("Perimeter " + circles[i].getPerimeter());
+            System.out.println();
+        }
+    }
+}
