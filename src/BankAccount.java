d5239a736218a0aa901a271ff5301a0d5b443ab3
Saeed Sekandari <sekandari2003@gmail.com>
2026-08-12 21:52:22 -0700

    Updated BankAccount info

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,70 +1,71 @@
 public class BankAccount {
-    private String accountNumber;
-    private String ownerName;
-    private double balance;
+  private String ownerName;
+  private String accountNumber;
+  private double balance;
 
-    static int numberOfAccounts;
+  private static int numOfAccountNumber;
 
-    public BankAccount(){
-        accountNumber = "Unknown";
-        ownerName = "Unknown";
-        balance = 0;
-        numberOfAccounts++;
+  //constructor to update bank account info
+  public BankAccount(){
+      ownerName = "unknown";
+      accountNumber = "unknown";
+      balance = 0;
+
+      numOfAccountNumber++;
+  }
+  //constructor to update the bank account info
+  public BankAccount(String ownerName, String accountNumber, double balance){
+      this.ownerName = ownerName;
+      this.accountNumber = accountNumber;
+      this.balance = balance;
+
+      numOfAccountNumber++;
+  }
+  //getter method to get the account number
+  public String getAccountNumber(){
+      return accountNumber;
+  }
+  //setter method to set the account number
+    public void  setAccountNumber(String newAccountNumber){
+      accountNumber = newAccountNumber;
     }
-
-    public BankAccount(String accountNumber, String ownerName, double balance){
-        this.accountNumber = accountNumber;
-        this.ownerName = ownerName;
-        this.balance = balance;
-        numberOfAccounts++;
-    }
-
-    public String getAccountNumber(){
-        return accountNumber;
-    }
-
+  //getter method to get the owner's name
     public String getOwnerName(){
-        return ownerName;
+      return ownerName;
     }
-
+    //setter method to update the owner name
+    public void setNewOwnerName(String newOwnerName){
+      ownerName = newOwnerName;
+    }
+    //getter method to get the balance
     public double getBalance(){
-        return balance;
+      return balance;
     }
-
-    public void setAccountNumber(String newAccountNumber){
-        accountNumber = newAccountNumber;
+    //deposit method
+    public void deposit( double amount){
+      if(amount>0){
+          balance+= amount;
+      }
+      else
+          System.out.println("Invalid amount!");
     }
-
-    public void setOwnerName(String newOwnerName){
-        ownerName = newOwnerName;
-    }
-    public void deposit(double amount){
-        if(amount > 0){
-            balance += amount;
-        }
-        else{
-            System.out.println("You need to deposit at least one cent!");
-        }
-    }
-    public static void depositMoney(BankAccount account, double amount) {
-        account.deposit(amount);
-    }
-
+    //withdraw method
     public void withdraw(double amount){
-        if(amount > 0 && amount<= balance){
-            balance -= amount;
-        }
-        else {
-            System.out.println("Not a valid withdraw!");
-        }
+      if(amount>0 && amount<= balance){
+          balance-= amount;
+      }
+      else
+          System.out.println("Invalid amount");
     }
-
-    public static int getNumOfAccounts(){
-        return numberOfAccounts;
-    }
-    public void displayAccount(){
-        System.out.println("Account Number: " + accountNumber);
-        System.out.println("Owner Name: " + ownerName);
+    //method to display the account info
+    public void display(){
+        System.out.println("Account number: " + accountNumber);
+        System.out.println("Owner name: " + ownerName);
         System.out.println("Balance: " + balance);
     }
+    //getter method to display the number of account created
+    public static int  getTotalNumOfAccount(){
+      return numOfAccountNumber;
+    }
+
 }
--- src/TestBankAccount.java
+++ src/TestBankAccount.java
@@ -1,51 +1,50 @@
 public class TestBankAccount {
 
-    public static void main(String[] args) {
-
-        // Create an array of BankAccount references
-        BankAccount[] accounts = new BankAccount[3];
-
-        // Create BankAccount objects
-        accounts[0] = new BankAccount("1001", "Saeed", 1000);
-        accounts[1] = new BankAccount("1002", "Ali", 500);
-        accounts[2] = new BankAccount("1003", "John", 2000);
-
-        // Display all accounts
-        for (int i = 0; i < accounts.length; i++) {
-            System.out.println("Account " + (i + 1));
-            accounts[i].displayAccount();
-            System.out.println();
-        }
-
-        // Deposit money
-        accounts[0].deposit(500);
-
-        // Withdraw money
-        accounts[1].withdraw(200);
-
-        // Pass an object to a method
-        depositMoney(accounts[2], 300);
-
-        // Reference variable
-        BankAccount anotherAccount = accounts[0];
-        anotherAccount.deposit(100);
-
-        // Display updated accounts
-        System.out.println("After transactions:");
-
-        for (int i = 0; i < accounts.length; i++) {
-            accounts[i].displayAccount();
-            System.out.println();
-        }
-
-        // Static method
-        System.out.println(
-                "Total number of accounts: "
-                        + BankAccount.getNumOfAccounts()
-        );
+    //Practice passing an object through method
+    public static void depositMoney(BankAccount bankAccounts, double amount ){
+        bankAccounts.deposit(amount);
     }
+    public static void main(String[] args) {
+        //Array of bank account references
+        BankAccount[] bankAccounts = new BankAccount[3];
+        //Create 3 bank accounts objects
+        bankAccounts[0] = new BankAccount("Saeed","1224435",1000000.44);
+        bankAccounts[1] = new BankAccount("Yasamin", "00042288555",1244433.3);
+        bankAccounts[2] = new BankAccount("Ali","3297744",10500.44);
 
-    public static void depositMoney(BankAccount account, double amount) {
-        account.deposit(amount);
+        //Display original accounts
+        for(int i=0; i< bankAccounts.length; i++ ){
+            System.out.println("Account: " + (i+1));
+            bankAccounts[i].display();
+            System.out.println();
+        }
+        //deposit normally
+        bankAccounts[0].deposit(20000);
+        System.out.println(bankAccounts[0].getOwnerName()+" Balance after first deposit: " + bankAccounts[0].getBalance());
+
+        //withdraw
+        bankAccounts[1].withdraw(500);
+
+        //two reference pointing at the same object
+        depositMoney(bankAccounts[0], 10000);
+        System.out.println(bankAccounts[0].getOwnerName()+" Balance after second deposit: " + bankAccounts[0].getBalance());
+        System.out.println();
+        bankAccounts[2].deposit(-388);
+        System.out.println();
+
+        //Display all accounts after transactions
+        for(int i=0; i< bankAccounts.length; i++){
+            bankAccounts[i].display();
+            System.out.println();
+        }
+
+        BankAccount account = new BankAccount();
+        account.setNewOwnerName("Zaher");
+        System.out.println("New account owner name: " + account.getOwnerName());
+        account.setAccountNumber("000000000");
+        System.out.println("New account number: " + account.getAccountNumber());
+
+        //show the total number of accounts
+        System.out.println("Total number of account created: " + BankAccount.getTotalNumOfAccount());
     }
 }
